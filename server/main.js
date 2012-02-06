3f1cfd485cc7580773a44f760ecbb4f5b18e1057
Alexander Brown <alex@alexanderdbrown.com>
2012-02-06 22:44:25 +0000

    Started on functionality

--- server/main.js
+++ server/main.js
@@ -1,3 +1,17 @@
 // Main Server Runtime.
 //TODO Write modules that will connect into this.
 
+var express = require('express');
+
+
+var server = express.createServer();
+
+server.set('view engine', 'jade');
+server.set('views', 'public/');
+
+server.get('/', function(req,res) {
+    res.render('index');
+  }
+)
+
+server.listen(1741);
